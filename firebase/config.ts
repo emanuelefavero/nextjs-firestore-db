2fe7af262d8222331c267042cb61595ab1ae40a5
emanuelefavero <emafaux89@gmail.com>
2023-02-01 03:01:56 +0100

    install and initialize firebase

---
+++ firebase/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC0bVJ9Exgcy6wxyGybu7d8zyPWWAkSK3Q',
+  authDomain: 'nextjs-firestore-db-df0dc.firebaseapp.com',
+  projectId: 'nextjs-firestore-db-df0dc',
+  storageBucket: 'nextjs-firestore-db-df0dc.appspot.com',
+  messagingSenderId: '779927272224',
+  appId: '1:779927272224:web:3b1618555713fc54fbf545',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
